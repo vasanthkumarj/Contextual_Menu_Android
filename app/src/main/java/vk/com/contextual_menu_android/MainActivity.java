d4f7597300f1c2313886e853a753fecee0862cc0
vasanth <vasanthkumar.j@outlook.com>
2019-10-18 23:53:13 +0530

    contextual menu action will delete list view elements

--- app/src/main/java/vk/com/contextual_menu_android/MainActivity.java
+++ app/src/main/java/vk/com/contextual_menu_android/MainActivity.java
@@ -1,18 +1,26 @@
 package vk.com.contextual_menu_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
-    private String[] android_version_list;
+    private String[] versionString;
+    private ArrayList<String> android_version_list = new ArrayList<>();
     private ArrayAdapter<String> adapter;
 
     @Override
@@ -21,7 +29,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.list_view);
         registerForContextMenu(listView);
-        android_version_list = getResources().getStringArray(R.array.android_version_array);
+        versionString = getResources().getStringArray(R.array.android_version_array);
+        for(String s : versionString)
+        {
+            android_version_list.add(s);
+        }
         adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.custom_list_view_layout,
                                      R.id.layout_text, android_version_list);
         listView.setAdapter(adapter);
@@ -32,5 +44,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.my_contextual_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo menuInfo= (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        if(item.getItemId()==R.id.delete)
+        {
+            android_version_list.remove(menuInfo.position);
+            adapter.notifyDataSetChanged();
+            return true;
+        }
+        return super.onContextItemSelected(item);
     }
 }
